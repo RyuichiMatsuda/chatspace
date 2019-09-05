5bca14140ac09fde5e40480cd89a940ca28b19f8
RyuichiMatsuda <pirorin2810@gmail.com>
2019-09-06 02:25:45 +0900

    インクメンタルサーチ実装

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,16 @@
 $(function () {
+  var search_list = $("#user-search-result");
+
+  function appendUserToSearchList(user) {
+    var html =
+      `<div class="chat-group-user clearfix">
+          <p class="chat-group-user__name">${user.name}</p>
+          <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name=${user.name}>追加</a>
+      </div>`
+    search_list.append(html);
+    return html;
+  }
+
   $("#user-search-field").on("keyup", function () {
     var input = $("#user-search-field").val();
     
@@ -7,7 +19,21 @@ $(function () {
       url: '/users',
       data: { keyword: input },
       dataType: 'json'
-      
+
+    })
+    .done(function (user) {
+      $("#user-search-result").empty();
+      if (user.length !== 0) {
+        user.forEach(function (user) {
+          appendUserToSearchList(user);
+        });
+      }
+      else {
+        appendNoUserToSearchList("メンバーがいません");
+      }
+    })
+    .fail(function () {
+      alert('ユーザー検索に失敗しました');
     })
   })
 })

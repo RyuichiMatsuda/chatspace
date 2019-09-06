caff1bfcd08c11677aab9b003e470f6669da9083
RyuichiMatsuda <pirorin2810@gmail.com>
2019-09-06 12:12:19 +0900

    削除機能は実装できた

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -18,8 +18,8 @@ $(function() {
       `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
         <input name='group[user_ids][]' type='hidden' value=${ user_id }>
         <p class='chat-group-user__name'>${ name }</p>
-        <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-      </div>`
+        <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+        </div>`
 　　 member_list.append(html);
   }
 

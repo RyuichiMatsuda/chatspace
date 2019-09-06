95691e4b1d7cb210efd52d44cd5509828792311f
RyuichiMatsuda <pirorin2810@gmail.com>
2019-09-06 14:58:30 +0900

    全て実装できた

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,15 +1,12 @@
 $(function () {
 
-  var search_list = $("#user-search-result");
-  var member_list = $(".js-add-user");
-
   function appendUserToSearch(user) {
     var html =
       `<div class="chat-group-user clearfix">
           <p class="chat-group-user__name">${ user.name}</p>
           <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id}" data-user-name=${user.name}>追加</a>
       </div>`
-    search_list.append(html);
+    $("#user-search-result").append(html);
     return html;
   }
 
@@ -20,10 +17,10 @@ $(function () {
       <div class='chat-group-user'>
       <input name='group[user_ids][]' type='hidden' value='${user_id}'>
       <p class='chat-group-user__name'>${name}</p>
-      <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</>
       </div>
       </div>`
-    member_list.append(html);
+    $(".js-add-user").append(html);
   }
 
   function appendNoUser(user) {
@@ -31,7 +28,7 @@ $(function () {
       `<div class="chat-group-user clearfix">
         <p class="chat-group-user__name">${ user}</p>
       </div>`
-    search_list.append(html);
+    $("#user-search-result").append(html);
   }
 
   $(function () {

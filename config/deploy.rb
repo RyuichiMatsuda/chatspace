9926e4cf9b6aa573ceba746c2ffdd39d34ce4e76
RyuichiMatsuda <pirorin2810@gmail.com>
2019-09-10 12:57:01 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '<2.5.1>'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['.ssh/ryu1028.pem']
+                  keys: ['/Users/ryu/.ssh/ryu1028.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
